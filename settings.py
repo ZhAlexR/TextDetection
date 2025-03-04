cf9c9ccf8a9a967f06d44c7102f5d65132ab2875
ZhAlexR <zhukov.o@ajax.systems>
2025-03-04 21:34:32 +0200

    Added base settings to load date from .env

---
+++ settings.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    AWS_ACCESS_KEY: str = "default-access-key"
+    AWS_SECRET_KEY: str = "default-secret-key"
+
+
+settngs = Settings()
