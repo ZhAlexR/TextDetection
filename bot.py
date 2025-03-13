f7dc49dea9f9e70591acb24980f4613bc6b95aa7
ZhAlexR <zhukov.o@ajax.systems>
2025-03-13 10:00:50 +0200

    Updated bot with options

--- bot.py
+++ bot.py
@@ -1,17 +1,29 @@
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 
 from settings import settings
 
-bot_client: Client = Client(
+bot: Client = Client(
     name="NutriBot",
     api_id=settings.TELEGRAM_API_ID,
     api_hash=settings.TELEGRAM_API_HASH,
     bot_token=settings.BOT_TOKEN,
 )
 
-@bot_client.on_message(filters.text & filters.private)
+
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton("Option1"),
+            KeyboardButton("Option2"),
+            KeyboardButton("Option3")
+        ]
+    ],
+    resize_keyboard=True,
+)
+
+@bot.on_message(filters.command("start"))
 async def echo(client, message):
-    await message.reply(message.text)
+    await message.reply("Chose option", reply_markup=menu)
 
-
-bot_client.run()
+bot.run()
